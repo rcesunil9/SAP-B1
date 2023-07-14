b770fa5bd23bd82ebfe8668ec98819612592b08c
DeveloperTwismi <111192388+DeveloperTwismi@users.noreply.github.com>
2023-07-14 21:23:57 +0530

    SalesQuotation Design Commit

---
+++ SAPWeb/Controllers/SalesQuotationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAPWeb.Controllers
+{
+    public class SalesQuotationController : Controller
+    {
+        // GET: SalesQuotation
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
